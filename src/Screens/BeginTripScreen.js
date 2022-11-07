14c7205c9748ec71a5c9ab9f0b4bd9dd9bdf8fab
Crystal Wen <wen00015@umn.edu>
2022-11-07 11:34:11 -0600

    Started Begin Trip Screen (Desktop 3)

---
+++ src/Screens/BeginTripScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Sidebar from '../Components/Sidebar';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+
+function BeginTripScreen(){
+    return (
+        <div classname = "MainScreen">
+            <Sidebar />
+
+        </div>
+    )
+}
+
+export default BeginTripScreen;
