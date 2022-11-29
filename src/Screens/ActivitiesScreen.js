3710a37a4a8493a4f3a60f41a653c10ef2861b15
Crystal Wen <wen00015@umn.edu>
2022-11-29 16:26:49 -0600

    Added backend for ActivitiesScreen

--- src/Screens/ActivitiesScreen.js
+++ src/Screens/ActivitiesScreen.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { setActivityTypes } from '../trips/trips.js';
 import Sidebar from '../Components/Sidebar';
 import { Col, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { Button, ToggleButton, ToggleButtonGroup } from 'react-bootstrap';
 
 function ActivitiesScreen(){
+    const [activities, setActivities] = React.useState(() => []);
+    const handleActivities = (newActivities) => {
+        setActivities(newActivities);
+    };
+
+    const handleSend = () => {
+        setActivityTypes(activities);
+    };
     return (
         <div className="MainScreen">
             <Row>
@@ -14,24 +23,24 @@ function ActivitiesScreen(){
                     <br />
 
                     <Row>
-                        <ToggleButtonGroup type="checkbox" className="mb-2">
-                            <ToggleButton variant = "outline-primary" id="Attractions" value={"Attractions"}>Attractions</ToggleButton>
-                            <ToggleButton variant = "outline-primary" id="Shopping" value={"Shopping"}>Shopping</ToggleButton>
-                            <ToggleButton variant = "outline-primary" id = "Outdoors" value={"Outdoors"}>Outdoors</ToggleButton>
+                        <ToggleButtonGroup type="checkbox" className="mb-2" value={activities} onChange={handleActivities}>
+                            <ToggleButton variant = "outline-primary" id="Attractions" value="Attractions">Attractions</ToggleButton>
+                            <ToggleButton variant = "outline-primary" id="Shopping" value="Shopping">Shopping</ToggleButton>
+                            <ToggleButton variant = "outline-primary" id = "Outdoors" value="Outdoors">Outdoors</ToggleButton>
                         </ToggleButtonGroup>
                         <br/>
                         
-                        <ToggleButtonGroup type="checkbox" className="mb-2">
-                            <ToggleButton variant = "outline-primary" id="Cuisine" value={"Cuisine"}>Cuisine</ToggleButton>
-                            <ToggleButton variant = "outline-primary" id="History" value={"History"}>History</ToggleButton>
-                            <ToggleButton variant = "outline-primary" id="Arts" value={"Arts"}>Arts</ToggleButton>
+                        <ToggleButtonGroup type="checkbox" className="mb-2" value={activities} onChange={handleActivities}>
+                            <ToggleButton variant = "outline-primary" id="Cuisine" value="Cuisine">Cuisine</ToggleButton>
+                            <ToggleButton variant = "outline-primary" id="History" value="History">History</ToggleButton>
+                            <ToggleButton variant = "outline-primary" id="Arts" value="Arts">Arts</ToggleButton>
                         </ToggleButtonGroup>
                         <br />
 
-                        <ToggleButtonGroup type="checkbox" className="mb-2">
-                            <ToggleButton variant = "outline-primary" id="Theatre" value={"Theatre"}>Theatre</ToggleButton>
-                            <ToggleButton variant = "outline-primary" id="Culture" value={"Culture"}>Culture</ToggleButton>
-                            <ToggleButton variant = "outline-primary" id="Wine" value={"Wine"}>Wine</ToggleButton>
+                        <ToggleButtonGroup type="checkbox" className="mb-2" value={activities} onChange={handleActivities}>
+                            <ToggleButton variant = "outline-primary" id="Theatre" value="Theatre">Theatre</ToggleButton>
+                            <ToggleButton variant = "outline-primary" id="Culture" value="Culture">Culture</ToggleButton>
+                            <ToggleButton variant = "outline-primary" id="Wine" value="Wine">Wine</ToggleButton>
                         </ToggleButtonGroup>
                     </Row>
                     <br />
@@ -44,7 +53,7 @@ function ActivitiesScreen(){
                         </Col>
                         <Col>
                             <Link to="/createtrip/vibes">
-                                <Button variant="outline-secondary" className='MainButton' id="to_vibes">Next</Button>{' '}
+                                <Button variant="outline-secondary" className='MainButton' id="to_vibes" onClick={handleSend} >Next</Button>{' '}
                             </Link>
                         </Col>
                         
